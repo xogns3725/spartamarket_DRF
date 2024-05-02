5fe3106aa992b4bde4680be11fe2172d97ce310e
xogns3725 <xogns3725@naver.com>
2024-05-02 11:18:24 +0900

    account delete func

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from django.contrib.auth.hashers import check_password
 from .models import User
 from .serializers import UserPasswordSerializer, UserSerializer
 
@@ -22,7 +23,6 @@ class UserListAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
     def put(self, request, username):
-        print(0)
         user = self.get_object(username)
         if request.user.username == user.username:
             serializer = UserSerializer(
@@ -31,7 +31,15 @@ class UserListAPIView(APIView):
                 serializer.save()
                 return Response(serializer.data)
             
-        
+    def delete(self, request):
+        user = get_object_or_404(User, username=request.user.username)
+        password = request.data.get('password')
+        if check_password(password, user.password):
+            user.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
 class UserUpdateAPIView(APIView):
     def put(self, request):
         user = get_object_or_404(User, username=request.user.username)
